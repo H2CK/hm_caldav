2e625bc224f928b8396e401c0ede5d70a6b79417
H2CK <H2CK@users.noreply.github.com>
2019-01-10 13:04:59 +0100

    Updated version number

--- hm_caldav.sh
+++ hm_caldav.sh
@@ -15,8 +15,8 @@
 # https://github.com/jens-maus/hm_pdetect
 #
 
-VERSION="0.3"
-VERSION_DATE="Dec 12 2018"
+VERSION="0.4"
+VERSION_DATE="Jan 10 2019"
 
 #####################################################
 # Main script starts here, don't modify from here on
